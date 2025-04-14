5693234fd747bd57df9915a7c2567c1fa7704166
abid-bin-ahosan <abid.ahosan@brainstation-23.com>
2025-04-14 14:00:42 +0600

    Final app to show on presentation.

---
+++ Services/NaturalSortHelper.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using System.Text.RegularExpressions;
+using TodoList.Models;
+
+namespace TodoList.Services
+{
+    public static class NaturalSortHelper
+    {
+        public static IOrderedQueryable<TodoItem> OrderByNatural(IQueryable<TodoItem> query)
+        {
+            return query.OrderBy(item => PadNumbers(item.Title));
+        }
+
+        public static IOrderedQueryable<TodoItem> OrderByDescendingNatural(IQueryable<TodoItem> query)
+        {
+            return query.OrderByDescending(item => PadNumbers(item.Title));
+        }
+
+        private static string PadNumbers(string input)
+        {
+            return Regex.Replace(input ?? "", @"\d+", match => match.Value.PadLeft(10, '0'));
+        }
+    }
+}
